32ef08af561922dc4ab67cb327a36e1c4c76f4e0
Emmanuel Bigeon <emmanuel@bigeon.fr>
2019-11-02 13:01:38 -0400

    Added comments

--- aggregator/src/main/java/net/bigeon/mcdas/aggregator/AggregatorType.java
+++ aggregator/src/main/java/net/bigeon/mcdas/aggregator/AggregatorType.java
@@ -11,13 +11,13 @@ package net.bigeon.mcdas.aggregator;
  * modify and/ or redistribute the software under the terms of the CeCILL
  * license as circulated by CEA, CNRS and INRIA at the following URL
  * "http://www.cecill.info".
- * 
+ *
  * As a counterpart to the access to the source code and  rights to copy,
  * modify and redistribute granted by the license, users are provided only
  * with a limited warranty  and the software's author,  the holder of the
  * economic rights,  and the successive licensors  have only  limited
  * liability.
- * 
+ *
  * In this respect, the user's attention is drawn to the risks associated
  * with loading,  using,  modifying and/or developing or reproducing the
  * software by the user in light of its specific status of free software,
@@ -28,15 +28,22 @@ package net.bigeon.mcdas.aggregator;
  * requirements in conditions enabling the security of their systems and/or
  * data to be ensured and,  more generally, to use and operate it in the
  * same conditions as regards security.
- * 
+ *
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  * #L%
  */
 
 /** The type of aggregators.
- * 
+ *
  * @author Emmanuel Bigeon */
 public enum AggregatorType {
-    MEAN, MAX, MIN, SUM;
+    /** Aggretator for the mean on aggregation period. */
+    MEAN,
+    /** Aggretator for the highest value on aggregation period. */
+    MAX,
+    /** Aggretator for the smallest value on aggregation period. */
+    MIN,
+    /** Aggretator for the sum of values on aggregation period. */
+    SUM;
 }
