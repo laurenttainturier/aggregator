35ed2baca9dffdba950996f221916132361724b7
Jean-Michel Coupal <jean-michel@coup.al>
2019-11-08 16:17:47 -0400

    Adds basic test implementation

---
+++ aggregator-socket/src/test/java/bigeon/mcdas/aggregator/socket/TestAdministrationServer.java
@@ -0,0 +1,1 @@
+package net.bigeon.mcdas.aggregator.socket;
---
+++ aggregator-socket/src/test/java/bigeon/mcdas/aggregator/socket/TestApp.java
@@ -0,0 +1,14 @@
+
+package bigeon.mcdas.aggregator.socket;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/** Main application */
+public class TestApp {
+  @Test
+  public void testLucky() {
+    assertEquals(7, 7);
+  }
+}
